62718b032284fb3f553556e48b69a2e34058eeaa
Dumitru Gorgan <dgorgan3@gmail.com>
2015-11-16 16:04:51 -0500

    created site logo, changed home index from doctors index to new pages controller that has home and about pages, and added gradient for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get '/about' => 'pages#about'
 
-  resources :doctors 
+
+  resources :doctors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
