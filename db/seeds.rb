2d5c1f40524cf756e1797e58e1d3f8aed1b9caf0
Dumitru Gorgan <dgorgan3@gmail.com>
2015-11-18 19:41:11 -0500

    integrated disqus_comments into doctors show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,20 @@ Doctor.destroy_all
 
 Doctor.create(name: "Patrictia Wellington", email: "patricia@doctors.com", photo:
 "http://blogs.lawyers.com/wp-content/uploads/2013/09/doctor-error-300.jpg", specialty:
-"Psychiatrist", description: "I have been praciticing psychiatry for over 25 years.
- You will not find a better psychiatrist to treat you!")
+"Psychiatrist", description: "I have been praciticing psychiatry for over 25 years. Bipolar,
+schizo, multiple-personality disorder. Whatever it is, i will cure it!")
 
 Doctor.create(name: "Bob Petterson", email: "bob@doctors.com", photo:
 "http://www.dpinjurylawyers.com/images/doctor-error.jpg", specialty:
-"Cardiologist", description: "I have been praciticing psychiatry for over 25 years.
- You will not find a better psychiatrist to treat you!")
+"Cardiologist", description: "Is your left arm going numb? Do you feel a tingling
+behind your shoulders, and is it runnin down the back of your spine? Holy shit mister,
+you're having a heart attack! NURSE! I need a defibrilator.")
 
-Doctor.create(name: "Ben Clifton", email: "bertha@doctors.com", photo:
+Doctor.create(name: "Scott Clifton", email: "bertha@doctors.com", photo:
 "http://hipjointpain.info/wp-content/uploads/2015/06/coxarthrosis-2.jpg", specialty:
-"P", description: "I have been praciticing psychiatry for over 25 years.
- You will not find a better psychiatrist to treat you!")
+"Dentist", description: "What do you mean I'm not a real doctor? I work just as hard
+as anyone else! Now I'm angry. My photo doesn't even represent my practice! Someone
+is about to get a root canal..WITHOUT anesthesia.")
 
 
 #   Mayor.create(name: 'Emanuel', city: cities.first)
